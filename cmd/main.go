c954e585260027f1e0415bd843cc5ba2ba200967
Award <awardmalisi@gmail.com>
2023-11-14 14:57:25 -0600

    testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"devOps00Boots/handlers/rest"
 	"log"
 	"net/http"
 )
@@ -9,35 +9,16 @@ import (
 func main() {
 
 	addr := ":8080"
+	//Sets the port to listen on❶
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			enc := json.NewEncoder(w)
-			w.Header().
-				Set("Content-Type",
-					"application/json; charset=utf-8")
-				// Sets the default header type since this will be a REST API
-
-			resp := Resp{
-				Language:    "English",
-				Translation: "Hello",
-			}
-			if err := enc.Encode(resp); err != nil {
-				panic(err)
-			}
-		})
+	mux.HandleFunc("/hello", rest.TranslateHandler)
+	//Registers the translation Handler❷
 
 	log.Printf("listening on %s\n", addr)
+	//Logs the listening port❸
 
 	log.Fatal(http.ListenAndServe(addr, mux))
-	// Runs the server
+	//Runs the server and logs if it fails
 }
-
-type Resp struct {
-	Language    string `json:"language"`
-	Translation string `json:"translation"`
-}
-
-// Common structure to store translation information
--- translation/translator.go
+++ translation/translator.go
@@ -1,5 +1,33 @@
-package translation        
- 
-func Translate(word string, language string) string {
-    return ""              
+package translation
+
+import "strings"
+
+func sanitizeInput(w string) string {
+	w = strings.ToLower(w)
+	return strings.TrimSpace(w)
 }
+
+func Translate(word string, language string) string {
+
+	word = sanitizeInput(word)
+	//Sanitizes incoming word
+	language = sanitizeInput(language)
+	//Sanitizes incoming language
+
+	if word != "hello" {
+		return ""
+	}
+	switch language {
+	case "english":
+		return "hello"
+	case "finnish":
+		return "hei"
+	case "german":
+		return "hallo"
+	default:
+		return ""
+	}
+}
+
+
+
